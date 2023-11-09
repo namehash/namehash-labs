ea6763b173c4f86d5739d5e2000991c3f2edc4be
eduramme <eduramme@gmail.com>
2023-11-09 00:11:13 -0300

    test

---
+++ components/2 - molecules/services-section.tsx
@@ -0,0 +1,178 @@
+import React from "react";
+import cc from "classcat";
+import {
+  ChartBarSquareIcon,
+  RocketLaunchIcon,
+  ShieldCheckIcon,
+  UserCircleIcon,
+} from "@heroicons/react/24/outline";
+import Image from "next/image";
+import {
+  ChartBarIcon,
+  ClockIcon,
+  HeartIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/24/solid";
+import { ExternalLinkIcon } from "../1 - atoms/icons/external-link-icon";
+
+interface ServiceProps {
+  label: {
+    icon: React.ReactElement;
+    title: string;
+  };
+  title: string;
+  subtitle: string;
+  illustration: React.ReactElement;
+  gradient?: React.ReactElement;
+  isInverted?: boolean;
+  buttonUrl?: string;
+}
+
+export const Service = ({
+  label,
+  title,
+  subtitle,
+  illustration,
+  gradient,
+  isInverted,
+  buttonUrl,
+}: ServiceProps) => {
+  return (
+    <section
+      className={cc([
+        "lg:py-20 lg:px-[120px] pt-10 pb-5 px-5 w-full flex flex-col items-center lg:flex-row",
+        {
+          "lg:flex-row-reverse": isInverted,
+        },
+      ])}
+    >
+      <div className="lg:w-1/2 w-full flex items-start justify-center">
+        <div className="flex-col inline-flex gap-5 lg:max-w-[568px]">
+          <div className="justify-center lg:justify-start flex">
+            <div className="gap-2 bg-black inline-flex items-center bg-opacity-5 px-4 py-2 rounded-[20px]">
+              {label.icon}
+              <p>{label.title}</p>
+            </div>
+          </div>
+
+          <h2 className="text-2xl leading-8 lg:text-4xl lg:leading-10 font-bold lg:text-start text-center">
+            {title}
+          </h2>
+          <p className="text-gray-500 text-lg leading-7 font-normal lg:text-start text-center">
+            {subtitle}
+          </p>
+          {buttonUrl && (
+            <div className="flex lg:justify-start justify-center">
+              <a
+                href={buttonUrl}
+                target="_blank"
+                className="border rounded-[8px] bg-black text-white px-4 py-2 transition-colors duration-200 hover:bg-gray-800 inline-flex items-center justify-center"
+              >
+                Learn more
+                <ExternalLinkIcon className="ml-3" />
+              </a>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="lg:w-1/2 w-full lg:mt-0 mt-5 relative flex items-center justify-center">
+        {gradient}
+        {illustration}
+      </div>
+    </section>
+  );
+};
+
+export const ServicesSection = () => {
+  return (
+    <>
+      {services.map((service, index) => (
+        <Service
+          key={service.title}
+          label={service.label}
+          title={service.title}
+          subtitle={service.subtitle}
+          illustration={service.illustration}
+          gradient={service.gradient}
+          isInverted={index % 2 === 1}
+          buttonUrl={service.buttonUrl}
+        />
+      ))}
+    </>
+  );
+};
+
+const services: ServiceProps[] = [
+  {
+    title: "NameKit",
+    subtitle:
+      "A fast, easy and highly customizable way for developers to add engaging ENS user journeys to their application.",
+    label: {
+      title: "Improved economics for building on ENS",
+      icon: <ChartBarSquareIcon className="h-5 w-5 text-gray-500" />,
+    },
+    illustration: (
+      <Image
+        quality={100}
+        width={568}
+        height={360}
+        src="/images/namekit-improved.png"
+        alt="hero"
+      />
+    ),
+  },
+  {
+    title: "ENS Ambassador Program",
+    subtitle:
+      "The power of a protocol lies not only in its technology,  but in the strength of its community. Passionate about ENS? Become an ambassador, help grow ENS, and start earning.",
+    label: {
+      title: "Incentive program to help ENS grow",
+      icon: <ChartBarSquareIcon className="h-5 w-5 text-gray-500" />,
+    },
+    illustration: (
+      <Image
+        quality={100}
+        width={568}
+        height={360}
+        src="/images/ens-incentive.png"
+        alt="hero"
+      />
+    ),
+  },
+  {
+    title: "NameGuard",
+    subtitle:
+      "Provide a fully secure environment for ENS names in your Web3 app, eliminate hidden risks for your users, and encourage optimal use of ENS. ",
+    label: {
+      title: "Protect the ENS community",
+      icon: <ShieldCheckIcon className="h-5 w-5 text-gray-500" />,
+    },
+    illustration: (
+      <Image
+        quality={100}
+        width={568}
+        height={360}
+        src="/images/nameguard-protect.png"
+        alt="hero"
+      />
+    ),
+  },
+  {
+    title: "ENSNode",
+    subtitle:
+      "Unlock new opportunities for ENS support in your web3 app with ENS Node, your gateway to a seamless fusion of richest on-chain and off-chain data from across the ENS ecosystem.",
+    label: {
+      title: "Developer API tooling",
+      icon: <ChartBarSquareIcon className="h-5 w-5 text-gray-500" />,
+    },
+    illustration: (
+      <Image
+        quality={100}
+        width={568}
+        height={360}
+        src="/images/ensnode-developer.png"
+        alt="hero"
+      />
+    ),
+  },
+];
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,53 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { DotPattern } from "@/components/1 - atoms/dot-pattenr";
+import { HeroTexture } from "@/components/1 - atoms/hero-texture";
+import { ServicesSection } from "@/components/2 - molecules/services-section";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col items-center justify-between">
+      <section className="h-screen w-full flex items-center justify-center relative bg-gray-200">
+        <div className="flex flex-col items-center gap-5 max-w-[728px] z-20">
+          <h1 className="text-5xl font-bold leading-[60px] text-center">
+            Core Infrastructure for the internet of identity
+          </h1>
+          <p className="text-lg text-center text-gray-500 font-normal leading-7">
+            We are a relentlessly committed team building open source public
+            goods to drive global adoption of the ENS protocol
+          </p>
+          <div>
+            <button className="text-white bg-black cursor-pointer py-[13px] hover:bg-gray-800 transition-colors duration-200 px-[25px] border rounded-[8px] flex">
+              Schedule a call
+            </button>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <HeroTexture className="absolute top-0 right-0 w-full h-full" />
+        <DotPattern className="absolute top-0 right-0 w-full h-full" />
+        <div className="bg-gradient-to-b from-transparent to-white h-1/2 w-full absolute bottom-0 left-0" />
+      </section>
+      <section className="px-[110px] py-[122px]">
+        <p
+          style={{
+            background: "linear-gradient(180deg, #289FB9 0%, #8740A0 100%)",
+            WebkitBackgroundClip: "text",
+            backgroundClip: "text",
+          }}
+          className="text-transparent text-4xl font-semibold text-center max-w-[1216px]"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          Founded in 2022, Namehash Labs is a technology organization dedicated
+          to infrastructure-level solutions that helps the Ethereum Name Service
+          (ENS) Protocol grow
+        </p>
+      </section>
+      <section>
+        <p className="text-xs font-medium leading-4 uppercase tracking-tight text-gray-500 text-center">
+          What we{"’"}re building
+        </p>
+        <h3 className="text-4xl leading-10 font-bold text-center mt-3">
+          Our products
+        </h3>
+      </section>
+      <ServicesSection />
     </main>
-  )
+  );
 }
