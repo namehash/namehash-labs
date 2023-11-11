ca0e4845edbd7db71c17f6b32777218788b97ba9
eduramme <eduramme@gmail.com>
2023-11-11 16:18:24 -0300

    trying to fix type error again

--- fix-type.ts
+++ fix-type.ts
@@ -1,18 +1,28 @@
 import * as fs from "fs";
+import * as path from "path";
 
 function fixType() {
-  const packageJsonRaw = fs.readFileSync(
-    "../../node_modules/@calcom/embed-react/package.json",
-    "utf8"
-  );
-  const packageJson = JSON.parse(packageJsonRaw);
+  try {
+    // Resolve the path to the package.json of @calcom/embed-react
+    const packageJsonPath = require.resolve("@calcom/embed-react/package.json");
+    const packageJsonDir = path.dirname(packageJsonPath);
 
-  packageJson.exports["."].types = "./dist/embed-react/src/index.d.ts";
+    // Read the package.json file
+    const packageJsonRaw = fs.readFileSync(packageJsonPath, "utf8");
+    const packageJson = JSON.parse(packageJsonRaw);
 
-  fs.writeFileSync(
-    "../../node_modules/@calcom/embed-react/package.json",
-    JSON.stringify(packageJson, null, 2)
-  );
+    // Modify the types path
+    packageJson.exports["."].types = "./dist/embed-react/src/index.d.ts";
+
+    // Write the modified package.json back
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+
+    console.log(
+      "Successfully fixed the type definitions for @calcom/embed-react"
+    );
+  } catch (error) {
+    console.error("Error while fixing type definitions:", error);
+  }
 }
 
 fixType();
