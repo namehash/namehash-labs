642da89984764439b078685bebd3e41031fe3622
eduramme <eduramme@gmail.com>
2023-11-12 12:09:54 -0300

    add namehashlabs logo link

--- components/2 - molecules/header.tsx
+++ components/2 - molecules/header.tsx
@@ -5,7 +5,10 @@ export const Header = () => {
   return (
     <div className="lg:px-[112px] px-5 w-full absolute top-0 left-0 border-b border-gray-300 z-20">
       <div className="w-full flex items-center justify-between py-5">
-        <NameHashLabsLogo />
+        <a href="https://namehashlabs.org/">
+          <NameHashLabsLogo />
+        </a>
+
         <CalButton className="px-[16px] py-[9px] bg-black text-white border rounded-[8px] text-sm leading-5 font-medium hover:bg-gray-900 transition-colors duration-200">
           Schedule a call
         </CalButton>
