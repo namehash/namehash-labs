ce27a3bd640ae3e333e3db9ce759c9a33e5add22
eduramme <eduramme@gmail.com>
2023-11-28 17:40:49 -0300

    whoops

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
           name="description"
