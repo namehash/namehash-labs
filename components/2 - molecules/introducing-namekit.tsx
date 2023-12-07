7fbe428b6c1aa03cb2c63a54ff6971307b611cbe
eduramme <eduramme@gmail.com>
2023-12-07 19:19:43 -0300

    create animations

--- components/2 - molecules/introducing-namekit.tsx
+++ components/2 - molecules/introducing-namekit.tsx
@@ -1,9 +1,7 @@
 import { PreSectionText, SectionText, SectionTitle } from "../1 - atoms";
-import Image from "next/image";
-import { EllipseBig } from "../1 - atoms/ellispse-big";
-import { EllipseMedium } from "../1 - atoms/ellispse-medium";
-import { EllipseSmall } from "../1 - atoms/ellispse-small";
 import { Balancer } from "react-wrap-balancer";
+import Lottie from "lottie-react";
+import waveAnimation from "./../../public/animation/wave-animation.json";
 
 export const IntroducingNamekit = () => {
   return (
@@ -18,12 +16,10 @@ export const IntroducingNamekit = () => {
           </Balancer>
         </SectionText>
       </div>
-      <Image
+      <Lottie
         className="relative max-w-[1216px] w-full h-auto"
-        src={"/images/building-image.png"}
-        alt="the vision"
-        width={1216}
-        height={520}
+        animationData={waveAnimation}
+        loop={true}
       />
     </section>
   );
