4e81f8f375d2ad4fe1ca1fcf010d17d557024156
eduramme <eduramme@gmail.com>
2024-01-05 16:55:17 -0300

    revert last changes - redirect is working

--- components/1 - atoms/join-our-team-section.tsx
+++ components/1 - atoms/join-our-team-section.tsx
@@ -58,7 +58,7 @@ export const JoinOurTeamSection = () => {
         <Image
           quality={100}
           alt="Join our team image"
-          src="/referral-hero.png"
+          src="/images/join-our-team.png"
           className="w-[335px] h-[231px] md:!min-w-[368px] md:!h-[254px] xl:!min-w-[588px] xl:!h-[400px]"
           height={800}
           width={1166}
