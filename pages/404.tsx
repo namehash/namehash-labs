ff55346c562fd50aee604c0584e93317df81c8ab
eduramme <eduramme@gmail.com>
2024-01-07 18:03:02 -0300

    create simple 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Custom404 = () => {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>This is a custom 404 page. You can put any content here.</p>
+    </div>
+  );
+};
+
+export default Custom404;
