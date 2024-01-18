dc40892b0675c9d9c663f1eaff39e7c458d08c32
eduramme <eduramme@gmail.com>
2024-01-18 06:13:51 -0300

    better style the page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,6 @@
 import { ENSServiceProviderBanner } from '@/components/1 - atoms';
+import { Title404 } from '@/components/1 - atoms/404-title';
+import { Footer } from '@/components/2 - molecules';
 import { Header } from '@/components/2 - molecules/header';
 import Link from 'next/link';
 import React from 'react';
@@ -13,25 +15,24 @@ const Custom404 = () => {
           <Header />
         </div>
         <div className="flex flex-col items-center  max-w-[600px] z-20 animate-fadeIn">
-          <h1 className="lg:text-[100px] text-5xl text-center font-bold lg:leading-[120px]">
-            404
-          </h1>
-          <h3>Page not found</h3>
-          <p className="text-lg text-center text-gray-500 font-normal leading-7 mt-4 max-w-[500px]">
+          <Title404
+            className='w-full max-w-]500px] mb-[60px]'
+          />
+          <h3 className='text-4xl leading-10 font-bold mb-4'>Page not found</h3>
+          <p className="text-sm leading-6 font-normal text-gray-500">
             <Balancer>
-              We integrate ENS onboarding into web3 wallets, apps, and games
+              We can’t seem to find the page you’re looking for
             </Balancer>
           </p>
-          <div className="flex items-center justify-center mt-10">
-            <Link 
-            href='/'
+          <div className="flex items-center justify-center mt-5">
+            <Link
+              href='/'
 
-            className="bg-black cursor-pointer py-[13px] hover:bg-gray-800 transition-colors duration-200 px-[25px] border rounded-[8px] flex text-sm leading-5 font-medium lg:text-base lg:leading-6 text-white">
-            Back to homepage
+              className="py-[9px] hover:bg-gray-200 transition-colors duration-200 px-[17px] border rounded-[8px] flex bg-white border-gray-300 lg:leading-6">
+              Back to homepage
             </Link>
           </div>
         </div>
-
         <div
           className="absolute bottom-0 left-0 w-full h-[140px] bg-white z-10"
           style={{
@@ -39,7 +40,6 @@ const Custom404 = () => {
               "linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, #FFF 100%)",
           }}
         />
-
         <div
           className="w-[100vw] h-[100vh] absolute opacity-70"
           style={{
@@ -48,7 +48,6 @@ const Custom404 = () => {
           }}
         />
         <div className="border rounded-full min-w-[1832px] min-h-[1832px] w-[120vw] h-[120vw] opacity-50 absolute bottom-0 translate-y-[50%] bg-white" />
-
         <div
           className="min-w-[1440px] min-h-[1440px] w-[95vw] h-[95vw] absolute bottom-0 translate-y-[50%]"
           style={{
@@ -57,8 +56,8 @@ const Custom404 = () => {
               "radial-gradient(50% 50% at 50% 50%, rgba(255, 255, 255, 0.00) 0%, rgba(255, 255, 255, 0.50) 48.96%)",
           }}
         />
-
       </section>
+      <Footer />
     </>
   );
 };
