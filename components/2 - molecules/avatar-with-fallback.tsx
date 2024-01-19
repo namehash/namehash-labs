ad3a62c77acef9bd1e65dcf962539a933f76aef7
frankind.eth <faguzzi3@gmail.com>
2024-01-19 09:16:48 -0300

    feat: [SC-23261] Updated font hierarchy inside Avatar tooltip

--- components/2 - molecules/avatar-with-fallback.tsx
+++ components/2 - molecules/avatar-with-fallback.tsx
@@ -91,7 +91,7 @@ export const AvatarWithFallback = ({
           </div>
           <div className="flex flex-col gap-1">
             <div className="flex gap-2 items-center ">
-              <p className="font-bold transition-all duration-200">
+              <p className="font-semibold text-lg transition-all duration-200">
                 {profile.ensName}
               </p>
 
@@ -105,9 +105,9 @@ export const AvatarWithFallback = ({
                   className="group"
                 >
                   <path
-                    d="M13.5222 10.7749L19.4785 4H18.0671L12.8952 9.88256L8.76437 4H4L10.2466 12.8955L4 20H5.41155L10.8732 13.7878L15.2356 20H20L13.5218 10.7749H13.5222ZM11.5889 12.9738L10.956 12.0881L5.92015 5.03974H8.0882L12.1522 10.728L12.7851 11.6137L18.0677 19.0075H15.8997L11.5889 12.9742V12.9738Z"
                     fill="#AFAFAF"
-                    className="fill-current text-gray-500 group-hover:text-white transition-color duration-200"
+                    d="M13.5222 10.7749L19.4785 4H18.0671L12.8952 9.88256L8.76437 4H4L10.2466 12.8955L4 20H5.41155L10.8732 13.7878L15.2356 20H20L13.5218 10.7749H13.5222ZM11.5889 12.9738L10.956 12.0881L5.92015 5.03974H8.0882L12.1522 10.728L12.7851 11.6137L18.0677 19.0075H15.8997L11.5889 12.9742V12.9738Z"
+                    className="fill-current text-gray-400 group-hover:text-white transition-color duration-200"
                   />
                 </svg>
               </a>
@@ -117,15 +117,19 @@ export const AvatarWithFallback = ({
                 target="_blank"
               >
                 <EnsSolidIcon
-                  fill="#666"
                   className="hover:brightness-200 transition"
-                  width={24}
+                  fill="#AFAFAF"
                   height={24}
+                  width={24}
                 />
               </a>
             </div>
-            {profile.displayName && <div>{profile.displayName}</div>}
-            <div className="max-w-[300px] text-gray-400">{profile.title}</div>
+            {profile.displayName && (
+              <p className="text-sm font-normal">{profile.displayName}</p>
+            )}
+            <div className="max-w-[300px] text-gray-400 text-sm">
+              {profile.title}
+            </div>
           </div>
         </div>
       </Tooltip>
