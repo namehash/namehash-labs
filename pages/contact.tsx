9c499b6b360b56db450b8b3c121f578cc7c996ce
frankind.eth <faguzzi3@gmail.com>
2024-01-23 15:27:28 -0300

    feat: [SC-22747] Added layout to Contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,5 +1,10 @@
 import { ContactSection } from "@/components/2 - molecules/contact-us-section";
+import { Layout } from "@/components/4 - templates/layout";
 
 export default function Contact() {
-  return <ContactSection />;
+  return (
+    <Layout>
+      <ContactSection />
+    </Layout>
+  );
 }
