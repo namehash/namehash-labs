77f33ab0d9fdb54fc08be8235b3a1fd110aa7b83
eduramme <eduramme@gmail.com>
2024-02-14 13:18:11 -0300

    change background color when loading

--- components/2 - molecules/contact-us-form.tsx
+++ components/2 - molecules/contact-us-form.tsx
@@ -297,7 +297,7 @@ export const ContactUsForm = ({title}: ContactUsFormProps) => {
                                     <button
                                         disabled={isLoading}
                                         type="submit"
-                                        className={cc([`rounded-lg px-4 py-2 text-center text-base font-medium text-white shadow-sm hover:bg-gray-800 w-full lg:w-auto duration-300 bg-black transition-all`])}
+                                        className={cc([`rounded-lg px-4 py-2 text-center text-base font-medium text-white shadow-sm hover:bg-gray-800 w-full lg:w-auto duration-300 transition-all`, isLoading ? "bg-gray-500" : "bg-black" ])}
                                     >
                                         {isLoading ? "Sending...": "Send message"}
                                     </button>
