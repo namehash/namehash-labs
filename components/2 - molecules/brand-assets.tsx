71c2c223a3d4e03ca535bb932ac07eaa15b31428
eduramme <eduramme@gmail.com>
2024-03-06 01:00:00 -0300

    create brand assets page content

--- components/2 - molecules/brand-assets.tsx
+++ components/2 - molecules/brand-assets.tsx
@@ -1,14 +1,40 @@
 /* eslint-disable @next/next/no-img-element */
-import { PreSectionText } from "../1 - atoms";
+import Image from "next/image";
+import { ExternalLinkIcon, PreSectionText } from "../1 - atoms";
 import { ColorfulBg } from "../1 - atoms/colorful-bg";
 
 export const BrandAssets = () => {
   return (
     <section className="w-full max-w-[1216px] py-20 px-5 h-full">
       <ColorfulBg className="absolute top-0 left-0 w-full z-[-1]" />
-      <div className="lg:mt-[100px] lg:mb-[120px] mt-[60px] mb-[60px]">
+      <div className="flex flex-col lg:mt-[100px] lg:mb-[120px] mt-[60px] mb-[60px] gap-3">
         <PreSectionText className="text-start ">Logos & MORE</PreSectionText>
-        <h1 className="font-bold text-[52px] ">Brand assets</h1>
+        <h1 className="font-bold lg:text-[52px] text-4xl leading-10">Brand assets</h1>
+      </div>
+
+      <div className="flex flex-col lg:flex-row gap-10 items-center justify-center border-t border-gray-200 py-20">
+        <div className="flex flex-col items-start gap-6">
+          <div className="flex flex-col gap-3">
+            <h3 className="text-3xl leading-9 lg:text-4xl lg:leading-10 font-bold">Download assets</h3>
+            <p className="text-lg leading-7 font-normal text-gray-500">
+              Working together with Namehash Labs or crafting an article
+              featuring us? Explore the official Namehash Labs assets available
+              on our Github to incorporate into your marketing materials,
+              website, or mobile application.
+            </p>
+          </div>
+          <button className="flex gap-3 flex-wrap bg-black rounded-[8px] text-white shadow-sm text-base leading-6 py-2 px-4 font-medium hover:bg-gray-800 transition-colors duration-200">
+            Download on Github <ExternalLinkIcon className="w-5 h-5" />
+          </button>
+        </div>
+        <div className="max-w-[588px] lg:flex-shrink-0">
+          <Image
+            width={588}
+            height={500}
+            src={"/images/brand-assets.png"}
+            alt={"Brand assets image"}
+          />
+        </div>
       </div>
     </section>
   );
