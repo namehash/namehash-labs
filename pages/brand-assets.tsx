03b0cd34f290e57ed28dd50213432557eadc6f3a
eduramme <eduramme@gmail.com>
2024-03-06 00:19:22 -0300

    create brand assets page

---
+++ pages/brand-assets.tsx
@@ -0,0 +1,10 @@
+import { BrandAssets } from "@/components/2 - molecules/brand-assets";
+import { Layout } from "@/components/4 - templates/layout";
+
+export default function Contact() {
+  return (
+    <Layout>
+      <BrandAssets />
+    </Layout>
+  );
+}
