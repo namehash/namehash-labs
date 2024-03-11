b0c6ef499d599e9c6ed2ebce3e2824eeed43817e
eduramme <eduramme@gmail.com>
2024-03-11 14:58:06 -0300

    create Partners cards

--- components/3 - organisms/contact-us-section.tsx
+++ components/3 - organisms/contact-us-section.tsx
@@ -1,11 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
-import {
-  CalButton,
-  PreSectionText,
-  SectionText,
-  SectionTitle,
-} from "../1 - atoms";
+import Balancer from "react-wrap-balancer";
+import { PreSectionText, SectionText, SectionTitle } from "../1 - atoms";
 import { ColorfulBg } from "../1 - atoms/colorful-bg";
+import { BlockfulLogo } from "../1 - atoms/icons/blockful-logo";
+import { EnelpolLogo } from "../1 - atoms/icons/enelpol-logo";
 import { ContactUsForm } from "../2 - molecules/contact-us-form";
 
 export const PartnersPage = () => {
@@ -23,12 +21,19 @@ export const PartnersPage = () => {
         <h3 className="text-4xl leading-10 font-bold">
           Let&apos;s work together
         </h3>
-        <p className="text-lg leading-7 font-normal">
+        <p className="text-lg leading-7 font-normal text-gray-500 max-w-[635px] text-center">
           We seek collaborations with other skilled teams and individuals who
           are passionate about helping ENS grow
         </p>
       </div>
-      <div className="w-full flex flex-col lg:flex-row m-auto gap-10 max-w-7xl">
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+        {partners.map((partner) => (
+          <PartnerCard key={partner.websiteUrl} {...partner} />
+        ))}
+      </div>
+
+      <div className="w-full flex pt-20 flex-col lg:flex-row m-auto gap-10 max-w-7xl">
         <div className="lg:w-1/2 w-full flex flex-col gap-3 items-start">
           <SectionTitle className="text-3xl lg:text-4xl font-bold">
             Become our partner
@@ -47,3 +52,55 @@ export const PartnersPage = () => {
     </section>
   );
 };
+
+interface PartnerCardProps {
+  title: React.ReactElement;
+  text: React.ReactElement;
+  websiteUrl: string;
+}
+
+const PartnerCard = ({ title, text, websiteUrl }: PartnerCardProps) => {
+  return (
+    <div className="flex flex-col items-start gap-6 justify-between py-8 px-10 border border-gray-300 rounded-[8px]">
+      <div className="flex flex-col items-start gap-6">
+        {title}
+        <p className="text-lg leading-7 font-normal text-gray-500">{text}</p>
+      </div>
+      <a
+        target="_blank"
+        href={websiteUrl}
+        className="py-2 px-4 rounded-[8px] border border-gray-300 shadow-sm hover:bg-gray-100 transition-colors duration-200"
+      >
+        Visit website
+      </a>
+    </div>
+  );
+};
+
+const partners: PartnerCardProps[] = [
+  {
+    title: <BlockfulLogo className="h-8" />,
+    text: (
+      <Balancer>
+        When it came time to build smart contracts for NameKit we chose to
+        collaborate with Blockful for their expertise in ENS. Together we{" "}
+        <span className="underline whitespace-nowrap">designed a solution</span>{" "}
+        that creates more financial incentives for wallets and dApps to
+        encourage registrations and renewals of .eth names and help ENS grow.
+      </Balancer>
+    ),
+    websiteUrl: "https://blockful.io/",
+  },
+  {
+    title: <EnelpolLogo className="h-8" />,
+    text: (
+      <Balancer>
+        We teamed up with Enelpol to create better name suggestions for NameKit
+        and organize collections in NameGraph. Using their Natural Language
+        Processing (NLP) skills and understanding of Unicode and fonts, Enelpol
+        helped improve NameGuard&apos;s features.
+      </Balancer>
+    ),
+    websiteUrl: "http://enelpol.com/",
+  },
+];
