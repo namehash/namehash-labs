012f49f58af6d584d783f30a958699b785075804
eduramme <eduramme@gmail.com>
2024-03-19 11:21:36 -0300

    Rename component

--- pages/partners.tsx
+++ pages/partners.tsx
@@ -1,7 +1,7 @@
 import { PartnersPage } from "@/components/3 - organisms/partners-section";
 import { Layout } from "@/components/4 - templates/layout";
 
-export default function Contact() {
+export default function Partners() {
   return (
     <Layout>
       <PartnersPage />
