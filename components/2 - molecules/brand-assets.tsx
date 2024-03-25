23084fde159e9e5f16f94349c0d6d1de51001e26
eduramme <eduramme@gmail.com>
2024-03-25 15:29:45 -0300

    feat: create brand assets download link

--- components/2 - molecules/brand-assets.tsx
+++ components/2 - molecules/brand-assets.tsx
@@ -9,13 +9,17 @@ export const BrandAssets = () => {
       <ColorfulBg className="absolute top-0 left-0 w-full z-[-1]" />
       <div className="flex flex-col lg:mt-[100px] lg:mb-[120px] mt-[60px] mb-[60px] gap-3">
         <PreSectionText className="text-start ">Logos & MORE</PreSectionText>
-        <h1 className="font-bold lg:text-[52px] text-4xl leading-10">Brand assets</h1>
+        <h1 className="font-bold lg:text-[52px] text-4xl leading-10">
+          Brand assets
+        </h1>
       </div>
 
       <div className="flex flex-col lg:flex-row gap-10 items-center justify-center border-t border-gray-200 py-20">
         <div className="flex flex-col items-start gap-6">
           <div className="flex flex-col gap-3">
-            <h3 className="text-3xl leading-9 lg:text-4xl lg:leading-10 font-bold">Download assets</h3>
+            <h3 className="text-3xl leading-9 lg:text-4xl lg:leading-10 font-bold">
+              Download assets
+            </h3>
             <p className="text-lg leading-7 font-normal text-gray-500">
               Working together with Namehash Labs or crafting an article
               featuring us? Explore the official Namehash Labs assets available
@@ -23,18 +27,29 @@ export const BrandAssets = () => {
               website, or mobile application.
             </p>
           </div>
-          <button className="flex gap-3 flex-wrap bg-black rounded-[8px] text-white shadow-sm text-base leading-6 py-2 px-4 font-medium hover:bg-gray-800 transition-colors duration-200">
+          <a
+            target="_blank"
+            href={"https://github.com/namehash/brand-assets"}
+            className="flex gap-3 flex-wrap bg-black rounded-[8px] text-white shadow-sm text-base leading-6 py-2 px-4 font-medium hover:bg-gray-800 transition-colors duration-200 "
+          >
             Download on Github <ExternalLinkIcon className="w-5 h-5" />
-          </button>
+          </a>
         </div>
-        <div className="max-w-[588px] lg:flex-shrink-0">
+        <a
+          href="/assets/brand-assets-main.zip"
+          download="brand-assets.zip"
+          className="max-w-[588px] lg:flex-shrink-0 relative group hover:cursor-pointer"
+        >
           <Image
             width={588}
             height={500}
-            src={"/images/brand-assets.png"}
+            src={"/images/brand-assets-card.png"}
             alt={"Brand assets image"}
           />
-        </div>
+          <div className="absolute flex items-center justify-center text-3xl left-1/2 bottom-[10%] shadow-lg w-[100px] h-[100px] bg-white rounded-full group-hover:scale-105 transition-all duration-200 ">
+            ⬇️
+          </div>
+        </a>
       </div>
     </section>
   );
