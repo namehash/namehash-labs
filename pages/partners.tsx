f6a53e9b37c7e8dc0da69e6351cce48dd4e9bb7f
eduramme <eduramme@gmail.com>
2024-03-25 20:13:24 -0300

    Feat: Include og tags in new phase 1 pages

--- pages/partners.tsx
+++ pages/partners.tsx
@@ -1,9 +1,18 @@
+import { SEO } from "@/components/1 - atoms/seo";
 import { PartnersPage } from "@/components/3 - organisms/partners-section";
 import { Layout } from "@/components/4 - templates/layout";
 
 export default function Partners() {
   return (
     <Layout>
+      <SEO
+        title="NameHash Labs - Partners"
+        description="NameHash Labs partners."
+        keywords="ens, web3, eth, namehash, partners"
+        ogImagePath="/images/partners-og-image.png"
+        ogImageAlt="NameHash Labs - Partners"
+        twitterImagePath="/images/partners-og-image.png"
+      />
       <PartnersPage />
     </Layout>
   );
