3664258e02fbcde2b0f794a9382276d4963c73a3
eduramme <eduramme@gmail.com>
2024-05-06 13:37:38 -0300

    redirect to 404 if not valid career path

--- app/careers/[slug]/page.tsx
+++ app/careers/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { Role } from "../../../types";
-
 import rolesData from "../../../data/rolesData";
 import { RolePage } from "@/components/3 - organisms/role-page";
+import { notFound } from "next/navigation";
 
 interface Props {
   params: { slug: string };
@@ -15,13 +15,7 @@ export default function Page({ params }: Props) {
   const role = rolesData.roles.find((role: Role) => role.slug === slug);
 
   if (!role) {
-    return (
-      <section className="w-full lg:py-20 pb-5 px-5 lg:px-[112px]">
-        <div className="max-w-[1216px] w-full m-auto pt-20 h-[calc(100% -20)]">
-          <h1 className="font-bold text-4xl text-center">Role not found</h1>
-        </div>
-      </section>
-    ); // Handle the case where no role matches the slug
+    notFound();
   }
 
   return (
