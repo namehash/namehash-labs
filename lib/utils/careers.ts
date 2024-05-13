0e8fa6cf71a065339a455e3d958c876dc0ea1530
eduramme <eduramme@gmail.com>
2024-05-13 20:59:53 -0300

    implement shuffle array function

--- lib/utils/careers.ts
+++ lib/utils/careers.ts
@@ -9,9 +9,12 @@ import { Role } from "@/types";
  * This effectively provides a random selection of roles related to the given role, up to the specified limit.
  *
  * @param role The role object to exclude from the returned list, ensuring only related roles are considered.
- * @param maxRelatedRoles The maximum number of related roles to return, which determines the size of the resulting array, this
- * number should never be greater than the total of roles (rolesData.roles.length).
- * @returns An array of Role objects that are related to the input role, randomly selected and limited to 'maxRelatedRoles' elements.
+ * @param allOpenRoles The array of all open roles available, which the function will filter and shuffle
+ * to generate the subset of related roles. This array should contain Role objects.
+ * @param maxRelatedRoles The maximum number of related roles to return, which determines the size of the resulting array.
+ * If maxRelatedRoles > allOpenRoles length then the actual max returned is allOpenRoles.length.
+ * @returns An array of Role objects that are related to the input role, randomly selected and limited to 'maxRelatedRoles' elements,
+ * it might return an array containing 0 elements.
  */
 
 export const getRelatedRoles = (
@@ -22,6 +25,20 @@ export const getRelatedRoles = (
   const filteredRoles = allOpenRoles.filter((item) => {
     return item.title !== role.title;
   });
-  const sortedRoles = filteredRoles.sort(() => 0.5 - Math.random());
-  return sortedRoles.slice(0, maxRelatedRoles);
+  const shuffledRoles = shuffleArray(filteredRoles);
+
+  return shuffledRoles.slice(0, maxRelatedRoles);
 };
+
+function shuffleArray<T>(array: T[]): T[] {
+  for (let i = array.length - 1; i > 0; i--) {
+    // Generate a random index from 0 to i
+    const j = Math.floor(Math.random() * (i + 1));
+
+    // Swap elements at indices i and j
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
