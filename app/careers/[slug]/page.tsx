b80f546c81a2d2b7d87b112186310ca95bcb34f2
eduramme <eduramme@gmail.com>
2024-05-13 21:30:33 -0300

    generate metadata dinamically for each career

--- app/careers/[slug]/page.tsx
+++ app/careers/[slug]/page.tsx
@@ -2,9 +2,37 @@ import { Role } from "../../../types";
 import rolesData from "../../../data/rolesData";
 import { RolePage } from "@/components/3 - organisms/role-page";
 import { notFound } from "next/navigation";
+import { Metadata } from "next";
 
 interface Props {
   params: { slug: string };
+}
+
+// Generate metadata dynamically
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = params;
+
+  const role = rolesData.roles.find((role: Role) => role.slug === slug);
+
+  if (!role) {
+    return {
+      title: "Role not found - NameHash Labs",
+      description: "The role you are looking for does not exist.",
+    };
+  }
+
+  return {
+    title: `${role.title} - NameHash Labs`,
+    description: `Join us as a ${role.title} at NameHash Labs.`,
+    keywords: [
+      "ens",
+      "web3",
+      "eth",
+      "namehash",
+      "careers",
+      role.title.toLowerCase(),
+    ],
+  };
 }
 
 export default function Page({ params }: Props) {
