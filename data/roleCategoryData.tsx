1e40eb297bbe21ea043ad68736364331488e8e2e
eduramme <eduramme@gmail.com>
2024-05-28 16:13:40 -0300

    create full stack category and role

--- data/roleCategoryData.tsx
+++ data/roleCategoryData.tsx
@@ -14,6 +14,7 @@ import {
   ContractsBanner,
   DesignBanner,
   FrontendBanner,
+  FullstackBanner,
   MarketingBanner,
   QualityAssuranceBanner,
   TalentBanner,
@@ -26,6 +27,7 @@ type RoleCategories =
   | "qualityassurance"
   | "contracts"
   | "marketing"
+  | "fullstack"
   | "general";
 
 const CategoriesData: Record<RoleCategories, RoleCategory> = {
@@ -58,6 +60,11 @@ const CategoriesData: Record<RoleCategories, RoleCategory> = {
     icon: MegaphoneIcon,
     name: "Marketing",
     banner: <MarketingBanner />,
+  },
+  fullstack: {
+    icon: StarIcon,
+    name: "Full Stack",
+    banner: <FullstackBanner />,
   },
   general: {
     icon: StarIcon,
