db16efbe08604ff374b0f41a80dc1c397f57a566
Eduardo <eduramme@gmail.com>
2024-05-30 08:42:59 -0300

    Update Support Banner to include Gitcoin (#88)

--- components/2 - molecules/financial-support-section.tsx
+++ components/2 - molecules/financial-support-section.tsx
@@ -1,5 +1,6 @@
 import { Balancer } from "react-wrap-balancer";
 import { EnsLogoColored } from "../1 - atoms/icons/ens-logo-colored";
+import { GitcoinLogo } from "../1 - atoms/icons/gitcoin-logo";
 
 export const FinancialSupportSection = () => {
   return (
@@ -8,7 +9,7 @@ export const FinancialSupportSection = () => {
       className="lg:px-[112px] w-full flex flex-col items-center justify-center px-5 py-20 "
     >
       <div
-        className="flex flex-col gap-5 items-center justify-center w-full max-w-[1216px] py-8 px-8 rounded-[8px] border border-gray-200"
+        className="flex flex-col gap-5 items-center justify-center w-full max-w-[1216px] py-8 px-5 rounded-[8px] border border-gray-200"
         style={{
           background:
             "linear-gradient(271deg, rgba(255, 255, 255, 0.00) 0%, #FFF 100%), linear-gradient(271deg, rgba(255, 175, 0, 0.15) 0%, rgba(241, 18, 217, 0.15) 32.29%, rgba(76, 63, 160, 0.15) 70.83%, rgba(46, 211, 198, 0.15) 95.83%)",
@@ -18,15 +19,24 @@ export const FinancialSupportSection = () => {
           role="text"
           className="text-2xl leading-8 font-semibold text-center"
         >
-          <Balancer>Our work receives generous support from ENS</Balancer>
+          <Balancer>Our work has received generous support from:</Balancer>
         </div>
-        <a
-          target="_blank"
-          href="https://ensdao.org/"
-          className="py-[9px] px-[25px] bg-white cursor-pointer hover:bg-gray-50 transition-colors duration-200 rounded-[8px] border border-gray-200"
-        >
-          <EnsLogoColored />
-        </a>
+        <div className="flex gap-3 items-center justify-center">
+          <a
+            target="_blank"
+            href="https://ensdao.org/"
+            className="py-[9px] px-[25px] bg-white cursor-pointer hover:bg-gray-50 transition-colors duration-200 rounded-[8px] border border-gray-200"
+          >
+            <EnsLogoColored className="w-full" />
+          </a>
+          <a
+            target="_blank"
+            href="https://www.gitcoin.co/"
+            className="py-[9px] px-[25px] bg-white cursor-pointer hover:bg-gray-50 transition-colors duration-200 rounded-[8px] border border-gray-200"
+          >
+            <GitcoinLogo className="w-full" />
+          </a>
+        </div>
       </div>
     </section>
   );
