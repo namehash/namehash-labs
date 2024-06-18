730d0d5c6c654abd10d5c01518d565ed21b82377
Eduardo <eduramme@gmail.com>
2024-06-18 05:45:06 -0300

    enable CORS on /api/contact-form (#91)

--- next.config.js
+++ next.config.js
@@ -15,6 +15,15 @@ const nextConfig = {
             { key: "Access-Control-Allow-Credentials", value: "true" },
             { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
           ]
+        },
+        {
+          source: "/api/contact-form",
+          headers: [
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET, POST"},
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
         }
     ]
   }
